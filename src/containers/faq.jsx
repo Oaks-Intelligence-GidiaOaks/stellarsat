0180fe00a6b36b9288c8f7ed78b693e6230c2934
Emmanuel otuonye <softtouchcomputers@SOFTTOUCHs-MacBook-Pro.local>
2023-11-07 22:16:30 +0100

    hjhjhj

--- src/containers/faq.jsx
+++ src/containers/faq.jsx
@@ -21,34 +21,34 @@ const FAQ = () => {
                 aid="panel1a-header"
                 ariaControls="panel1a-content"
                 heading="How Long Does it Take to Complete a Project?"
-                paragraph="Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta eaque est eum ratione ex, quia enim exercitationem aut assumenda explicabo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique eligendi excepturi minus tenetur consequuntur velit earum non libero, possimus, explicabo quasi ipsam eos? Sapiente ratione maxime, ipsam laborum recusandae beatae."
+                paragraph="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."
               />
 
               <FaqCard
                 aid="panel2a-header"
                 ariaControls="panel2a-content"
                 heading="Where is Stellarsat Located?"
-                paragraph="Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta eaque est eum ratione ex, quia enim exercitationem aut assumenda explicabo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique eligendi excepturi minus tenetur consequuntur velit earum non libero, possimus, explicabo quasi ipsam eos? Sapiente ratione maxime, ipsam laborum recusandae beatae."
+                paragraph="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."
               />
 
               <FaqCard
                 heading="What is the refund policy?"
-                paragraph="Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta eaque est eum ratione ex, quia enim exercitationem aut assumenda explicabo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique eligendi excepturi minus tenetur consequuntur velit earum non libero, possimus, explicabo quasi ipsam eos? Sapiente ratione maxime, ipsam laborum recusandae beatae."
+                paragraph="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."
               />
 
               <FaqCard
                 heading="Will there be a discount for returning clients?"
-                paragraph="Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta eaque est eum ratione ex, quia enim exercitationem aut assumenda explicabo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique eligendi excepturi minus tenetur consequuntur velit earum non libero, possimus, explicabo quasi ipsam eos? Sapiente ratione maxime, ipsam laborum recusandae beatae."
+                paragraph="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."
               />
 
               <FaqCard
                 heading="Do I need to Pay Before My Project Starts?"
-                paragraph="Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta eaque est eum ratione ex, quia enim exercitationem aut assumenda explicabo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique eligendi excepturi minus tenetur consequuntur velit earum non libero, possimus, explicabo quasi ipsam eos? Sapiente ratione maxime, ipsam laborum recusandae beatae."
+                paragraph="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."
               />
 
               <FaqCard
                 heading="How Can I Be Updated on the Progress of my Project?"
-                paragraph="Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta eaque est eum ratione ex, quia enim exercitationem aut assumenda explicabo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique eligendi excepturi minus tenetur consequuntur velit earum non libero, possimus, explicabo quasi ipsam eos? Sapiente ratione maxime, ipsam laborum recusandae beatae."
+                paragraph="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."
               />
             </div>
           </div>
